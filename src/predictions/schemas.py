32e83bfa4420bd6ec0b9520df155d2a21a860584
nhtbao2491 <nhtbao2491@gmail.com>
2026-03-14 07:48:31 +0700

    Chỉnh sửa các file Schemas.py

--- src/predictions/schemas.py
+++ src/predictions/schemas.py
@@ -1,16 +1,16 @@
 from pydantic import BaseModel, Field
 
 class PredictionRequest(BaseModel):
-    area: float = Field(..., examples=120.3, description="Diện tích nhà (m2)")
-    bedrooms: int = Field(..., examples=4, description="Số phòng ngủ")
-    bathrooms: int = Field(..., examples=4, description="Số phòng tắm")
-    floors: int = Field(..., examples=3, description="Số tầng lầu")
-    property_type: str = Field(..., examples="Villa", description="Loại bất động sản")
-    furniture: str = Field(..., examples="Basic", description="Tình trạng nội thất")
-    legal_status: str = Field(..., examples="Contract", description="Tình trạng pháp lý")
-    distance_to_center: int = Field(..., examples="10", description="Khoảng cách đến trung tâm (km)")
+    area: float = Field(..., examples=[120.3], description="Diện tích nhà (m2)")
+    bedrooms: int = Field(..., examples=[4], description="Số phòng ngủ")
+    bathrooms: int = Field(..., examples=[4], description="Số phòng tắm")
+    floors: int = Field(..., examples=[3], description="Số tầng lầu")
+    property_type: str = Field(..., examples=["Villa"], description="Loại bất động sản")
+    furniture: str = Field(..., examples=["Basic"], description="Tình trạng nội thất")
+    legal_status: str = Field(..., examples=["Contract"], description="Tình trạng pháp lý")
+    distance_to_center: int = Field(..., examples=[10], description="Khoảng cách đến trung tâm (km)")
     
 
 class PredictionResponse(BaseModel):
-    price: float = Field(..., examples=133232.33, description="Giá nhà dự đoán")
+    price: float = Field(..., examples=[133232.33], description="Giá nhà dự đoán")
     
